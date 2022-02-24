bd603b907e7dbc159ff2b981a3c71d57d3e9daf4
Gui Barscevicius <guilbrs@gmail.com>
2022-02-24 16:00:06 -0300

    reorganize and enhance code

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -9,33 +9,46 @@ export const fetchPlugin = (input: string) => {
   return {
     name: 'fetch-plugin',
     setup(build: esbuild.PluginBuild) {
-      build.onLoad({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') {
-          return {
-            loader: 'jsx',
-            contents: input,
-          }
-        }
+      build.onLoad({ filter: /^index\.js$/ },
+        () => ({ loader: 'jsx', contents: input }))
 
-        // const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
-        // if (cached) return cached
+      build.onLoad({ filter: /.css$/ }, async (args: any) => {
+        const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+        if (cached) return cached
 
         const response = await fetch(args.path)
         const data = await response.text()
 
-        const contents = args.path.match(/.css$/)
-          ? `
+        const contents = `
             const style = document.createElement('style')
             style.innerText = '${data
               .replace(/\n/g, '')
               .replace(/"/g, '\\')
               .replace(/'/g, "\\'")
             }'
-          ` : data
+          `
 
         const result: esbuild.OnLoadResult = {
           loader: 'jsx',
           contents,
+          resolveDir: new URL('./', response.url).pathname
+        }
+
+        await fileCache.setItem(args.path, result)
+
+        return result
+      })
+
+      build.onLoad({ filter: /.*/ }, async (args: any) => {
+        const cached = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
+        if (cached) return cached
+
+        const response = await fetch(args.path)
+        const data = await response.text()
+
+        const result: esbuild.OnLoadResult = {
+          loader: 'jsx',
+          contents: data,
           resolveDir: new URL('./', response.url).pathname
         }
 
