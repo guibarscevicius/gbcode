a58598b9f5970458f07c6f410f345720516c6d82
Gui Barscevicius <guilbrs@gmail.com>
2022-02-24 15:19:55 -0300

    implement textarea, enhance code

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -7,35 +7,39 @@ const fileCache = localForage.createInstance({
   name: 'filecache'
 })
      
-export const unpkgPathPlugin = () => {
+export const unpkgPathPlugin = (input: string) => {
   return {
     name: 'unpkg-path-plugin',
 
     setup(build: esbuild.PluginBuild) {
-      build.onResolve({ filter: /.*/ }, async (args: any) => {
-        if (args.path === 'index.js') return { path: args.path, namespace: 'a' }
+      build.onResolve(
+        { filter: /(^index\.js)/ },
+        () => ({ path: 'index.js', namespace: 'a' })
+      )
 
-        if(args.path.includes('./') || args.path.includes('../')) {
+      build.onResolve(
+        { filter: /^\.+\// },
+        (args: any) => ({
+          namespace: 'a',
+          path: new URL(args.path, baseUrl + args.resolveDir + '/').href
+        })
+      )
+
+      build.onResolve(
+        { filter: /.*/ },
+        async (args: any) => {
           return {
             namespace: 'a',
-            path: new URL(args.path, baseUrl + args.resolveDir + '/').href
+            path: baseUrl + '/' + args.path
           }
         }
-
-        return {
-          namespace: 'a',
-          path: baseUrl + '/' + args.path
-        }
-      })
+      )
  
       build.onLoad({ filter: /.*/ }, async (args: any) => {
         if (args.path === 'index.js') {
           return {
             loader: 'jsx',
-            contents: `
-              import message from 'react'
-              console.log(message)
-            `,
+            contents: input,
           }
         }
 
