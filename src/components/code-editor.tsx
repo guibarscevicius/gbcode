7632a115a908ef0f193b51b86ef013e774a7ee26
Gui Barscevicius <guilbrs@gmail.com>
2022-03-07 14:21:49 -0300

    implement prettier

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,8 @@
+import { useRef } from 'react'
 import MonacoEditor from '@monaco-editor/react'
 import monaco from 'monaco-editor'
+import prettier from 'prettier'
+import parser from 'prettier/parser-babel'
 
 interface CodeEditorProps {
   initialValue: string,
@@ -7,25 +10,50 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue, onChange }) => {
+  const editorRef = useRef<any>()
+
+  const onFormatClick = () => {
+    const unformatted = editorRef.current.getModel().getValue()
+
+    const formatted = prettier.format(unformatted, {
+      parser: 'babel',
+      plugins: [parser],
+      useTabs: false,
+      semi: false,
+      singleQuote: true,
+    })
+    
+    editorRef.current.setValue(formatted)
+  }
+
+  const onEditorMount = (editor: monaco.editor.IStandaloneCodeEditor) => {
+    editorRef.current = editor
+  }
+
   return (
-    <MonacoEditor
-      value={initialValue}
-      height="500px"
-      theme="vs-dark"
-      defaultLanguage="javascript"
-      options={{
-        wordWrap: 'on',
-        minimap: { enabled: false },
-        showUnused: false,
-        folding: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-        tabSize: 2,
-      }}
-      onChange={onChange}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      
+      <MonacoEditor
+        value={initialValue}
+        height="500px"
+        theme="vs-dark"
+        defaultLanguage="javascript"
+        options={{
+          wordWrap: 'on',
+          minimap: { enabled: false },
+          showUnused: false,
+          folding: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+        onMount={onEditorMount}
+        onChange={onChange}
+      />
+    </div>
   )
 }
 
