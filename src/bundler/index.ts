14d5e42b2fbd718e9a9e3449859a5d12f4472b76
Gui Barscevicius <guilbrs@gmail.com>
2022-03-08 09:31:13 -0300

    split bundler

---
+++ src/bundler/index.ts
@@ -0,0 +1,29 @@
+import esbuild from 'esbuild-wasm'
+
+import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin'
+import { fetchPlugin } from '../plugins/fetch-plugin'
+import { cachePlugin } from '../plugins/cache-plugin'
+
+(async () => {
+  await esbuild.initialize({
+    worker: true,
+    wasmURL: 'https://unpkg.com/esbuild-wasm@0.14.23/esbuild.wasm'
+  })
+})()
+
+const bundler = async (rawCode: string) => {
+  const { outputFiles: [ outputFile ] } = await esbuild.build({
+    entryPoints: ['index.js'],
+    bundle: true,
+    write: false,
+    plugins: [unpkgPathPlugin(), cachePlugin(), fetchPlugin(rawCode)],
+    define: {
+      'process.env.NODE_ENV': '"production"',
+      global: 'window',
+    },
+  })
+
+  return outputFile.text
+}
+
+export default bundler
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,43 +1,19 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import esbuild from 'esbuild-wasm'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 import CodeEditor from './code-editor'
 import Preview from './preview'
-
-import { unpkgPathPlugin } from '../plugins/unpkg-path-plugin'
-import { fetchPlugin } from '../plugins/fetch-plugin'
-import { cachePlugin } from '../plugins/cache-plugin'
+import bundle from '../bundler'
 
 const App = () => {
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const startService = async () => {
-    await esbuild.initialize({
-      worker: true,
-      wasmURL: 'https://unpkg.com/esbuild-wasm@0.14.23/esbuild.wasm'
-    })
-  }
-
-  useEffect(() => { startService() }, [])
+  
 
   const onClick = async () => {
-    const result = await esbuild.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), cachePlugin(), fetchPlugin(input)],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window',
-      },
-    })
-
-    setCode(result.outputFiles[0].text)
-  }
-
-  
+    setCode(await bundle(input))
+  }  
 
   return (
     <div>
